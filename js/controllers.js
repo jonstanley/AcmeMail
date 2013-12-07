5fa157017801fa0d50b5db686f2cc46fe42595f2
Jonathan Stanley <jon@lambcutlet.org>
2013-12-07 01:59:07 +0000

    Some uncommitted changes...

--- js/controllers.js
+++ js/controllers.js
@@ -14,9 +14,13 @@ acmeMsgAppControllers.controller('DashboardCtrl', function($scope, $http) {
 	$scope.messages = data;
 	});
 	$scope.orderProp = Array('-todo', 'name.last');
+	$scope.go = function(viewMsg) {
+          var hash = '/message/';
+		  alert(hash);
+        };
 });
 
-acmeMsgAppControllers.controller('SingleMsgCtrl', function($scope, $http) {
+acmeMsgAppControllers.controller('SingleMsgCtrl', function($scope, $http, $routeParams) {
 	$http.get('/db/messages.json').success(function(data) {
 	$scope.messages = data;
 	});
